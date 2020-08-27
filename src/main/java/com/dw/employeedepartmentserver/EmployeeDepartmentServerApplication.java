ee97a740e54759d13bdab48585e62d6665f32b37
44dw <elevation1987@gmail.com>
2020-08-27 08:00:53 +0400

    add hystrix

--- src/main/java/com/dw/employeedepartmentserver/EmployeeDepartmentServerApplication.java
+++ src/main/java/com/dw/employeedepartmentserver/EmployeeDepartmentServerApplication.java
@@ -2,11 +2,15 @@ package com.dw.employeedepartmentserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dw")
 @EnableEurekaServer
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableFeignClients(basePackages = {"com.dw.departmentdictionary.api.feign"})
 public class EmployeeDepartmentServerApplication {
 
---
+++ src/main/java/com/dw/employeedepartmentserver/config/EmployeeDepartmentConfig.java
@@ -0,0 +1,8 @@
+package com.dw.employeedepartmentserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmployeeDepartmentConfig {
+
+}
